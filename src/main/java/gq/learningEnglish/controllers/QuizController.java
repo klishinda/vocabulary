75f1fa550e45019d2575a784a97276fdaa759ecf
tronix <klishin.d.a@yandex.ru>
2019-10-19 15:11:42 +0300

    Adding recording results into database. Small refactoring

--- src/main/java/gq/learningEnglish/controllers/QuizController.java
+++ src/main/java/gq/learningEnglish/controllers/QuizController.java
@@ -1,83 +1,31 @@
 package gq.learningEnglish.controllers;
 
 import gq.learningEnglish.daos.HistoryDao;
-import gq.learningEnglish.daos.PrepareQuestionsDao;
 import gq.learningEnglish.models.RandomWordsMode;
 import gq.learningEnglish.models.wordsQuestionnaire.Answer;
 import gq.learningEnglish.models.wordsQuestionnaire.Question;
+import gq.learningEnglish.services.AskingVocabulary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 @Controller
 public class QuizController {
-    private final PrepareQuestionsDao prepareQuestionsDao;
+    private final AskingVocabulary askingVocabulary;
     private final HistoryDao historyDao;
 
-    private String answer;
-    private Scanner scanner = new Scanner(System.in);
-
-    public QuizController(PrepareQuestionsDao prepareQuestionsDao, HistoryDao historyDao) {
-        this.prepareQuestionsDao = prepareQuestionsDao;
+    public QuizController(AskingVocabulary askingVocabulary, HistoryDao historyDao) {
+        this.askingVocabulary = askingVocabulary;
         this.historyDao = historyDao;
     }
 
     @Bean
     public void startTest() {
-        RandomWordsMode wordsMode = RandomWordsMode.RUSSIAN;
-        Map<Question, List<Answer>> askingWords = prepareQuestionsDao.getRandomWords(6, wordsMode);
-        System.out.println("Let's start! Write translation to the next words." + askingWords.size());
-
-        String tempResult;
-        int countCorrectAnswers = 0;
-        int countAllWords = 0;
-        // set only correct answers. All empty fields "result" means wrong answer
-        for (Map.Entry<Question, List<Answer>> wordMap : askingWords.entrySet()) {
-            Question questionWord = wordMap.getKey();
-            System.out.println(questionWord.getAskingWord());
-            for (int i = 0; i < wordMap.getValue().size(); i++) {
-                countAllWords++;
-                System.out.println("Your answer: ");
-                answer = scanner.nextLine().toUpperCase();
-                if (wordMap.getValue().stream().anyMatch(s -> s.getAnswerWord().equals(answer) && !s.isResult())) {
-                    wordMap.getValue().stream().filter(s -> s.getAnswerWord().equals(answer)).forEach(ss -> ss.setResult(true));
-                    tempResult = "CORRECT!";
-                    countCorrectAnswers++;
-                } else {
-                    tempResult = "INCORRECT!";
-                }
-                System.out.println("Your answer is " + answer + ". " + tempResult);
-            }
-        }
-        System.out.println("----------RESULTS----------");
-        System.out.println("Correct answers: "+countCorrectAnswers+"/"+countAllWords);
+        RandomWordsMode wordsMode = RandomWordsMode.ABSOLUTE_RANDOM;
+        int randomWords = 5;
+        Map<Question, List<Answer>> quizResults = askingVocabulary.quiz(wordsMode, randomWords);
+        historyDao.addHistoryRecords(quizResults);
     }
-
-    /*private void askingRussianTranslation(int i) {
-        System.out.println(askingWords.get(i).getRussianWord() + ": ");
-        answer = scanner.nextLine().toUpperCase();
-        if (answer.equals(askingWords.get(i).getEnglishWord())) {
-            askingWords.get(i).setResult(true);
-            tempResult = "CORRECT!";
-        } else {
-            askingWords.get(i).setResult(false);
-            tempResult = "INCORRECT!";
-        }
-        askingWords.get(i).setAskingLanguage(AvailableLanguages.RUSSIAN);
-        System.out.println("Your answer is " + answer + ". " + tempResult);
-    }
-
-    private void askingEnglishTranslation(int i) {
-        askingWords.get(i).setAskingLanguage(AvailableLanguages.ENGLISH);
-        if (answer.equals(askingWords.get(i).getRussianWord())) {
-            askingWords.get(i).setResult(true);
-            tempResult = "CORRECT!";
-        } else {
-            askingWords.get(i).setResult(false);
-            tempResult = "INCORRECT!";
-        }
-    }*/
 }
--- src/main/java/gq/learningEnglish/daos/HistoryDao.java
+++ src/main/java/gq/learningEnglish/daos/HistoryDao.java
@@ -2,10 +2,14 @@ package gq.learningEnglish.daos;
 
 import gq.learningEnglish.SqlScripts;
 import gq.learningEnglish.models.WordsForTesting;
+import gq.learningEnglish.models.wordsQuestionnaire.Answer;
+import gq.learningEnglish.models.wordsQuestionnaire.Question;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class HistoryDao {
@@ -22,5 +26,23 @@ public class HistoryDao {
         params.put("lang", wordsForTesting.getAskingLanguage().name());
         params.put("result", wordsForTesting.getResult());
         jdbc.update(SqlScripts.ADD_HISTORY_RESULT.getSql(), params);
+    }
+
+    public void addHistoryRecords(Map<Question, List<Answer>> results) {
+        final HashMap<String, Object> params = new HashMap<>();
+        for (Map.Entry<Question, List<Answer>> entry : results.entrySet()) {
+            params.put("userId", 101);
+            params.put("lang", entry.getKey().getAskingLanguage().name());
+            for (Answer a: entry.getValue()) {
+                if (a.isResult()) {
+                    params.put("result", true);
+                }
+                else {
+                    params.put("result", false);
+                }
+                params.put("vocabularyId", a.getVocabularyId());
+                jdbc.update(SqlScripts.ADD_HISTORY_RESULT.getSql(), params);
+            }
+        }
     }
 }
